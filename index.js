027e18e645992d20c572bdabe10ababe97193812
AnikaBushra <anikabushra024@gmail.com>
2021-10-29 10:00:09 +0600

    added hello api

--- index.js
+++ index.js
@@ -64,6 +64,9 @@ run().catch(console.dir);
 app.get('/', (req, res) => {
     res.send('Running genius server');
 });
+app.get('/hello', (req, res) => {
+    res.send('hello updated here');
+})
 
 app.listen(port, () => {
     console.log('running genius server on port', port);
